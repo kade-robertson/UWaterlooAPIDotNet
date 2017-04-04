a22fc0eb636e78574eb5655dcb449e9441a40782
kade-robertson <kade.robertson96@gmail.com>
2017-04-04 07:55:06 -0400

    finished documenting API models and module

--- src/UWaterlooAPIDotNet/Models/API/Usage.cs
+++ src/UWaterlooAPIDotNet/Models/API/Usage.cs
@@ -4,15 +4,27 @@ namespace UWaterlooAPIDotNet.Models.API
 {
     public class Usage
     {
+        /// <summary>
+        /// The name related to the API key.
+        /// </summary>
         [JsonProperty(PropertyName = "name")]
         public string Name { get; private set; }
 
+        /// <summary>
+        /// The API key the usage statistics apply to.
+        /// </summary>
         [JsonProperty(PropertyName = "api_key")]
         public string APIKey { get; private set; }
 
+        /// <summary>
+        /// The number of calls made in the current month by the given API key.
+        /// </summary>
         [JsonProperty(PropertyName = "monthly_calls")]
         public int? MonthlyCalls { get; private set; }
 
+        /// <summary>
+        /// The number of calls made in total by the given API key.
+        /// </summary>
         [JsonProperty(PropertyName = "total_calls")]
         public int? TotalCalls { get; private set; }
     }
--- src/UWaterlooAPIDotNet/Models/API/Version.cs
+++ src/UWaterlooAPIDotNet/Models/API/Version.cs
@@ -5,9 +5,15 @@ namespace UWaterlooAPIDotNet.Models.API
 {
     public class Version
     {
+        /// <summary>
+        /// The version number as a decimal.
+        /// </summary>
         [JsonProperty(PropertyName = "version")]
         public decimal? VersionNumber { get; private set; }
 
+        /// <summary>
+        /// The date the version was released.
+        /// </summary>
         [JsonProperty(PropertyName = "release_date")]
         public DateTime? ReleaseDate { get; private set; }
     }
